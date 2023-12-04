57d0c4362b7fefe61b020ce38fb30a19259658ba
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2023-12-04 13:04:44 -0800

    finished creating new react app

---
+++ intro-hiro-garry-leo/src/components.js
@@ -0,0 +1,28 @@
+import React from 'react'
+const Components = () => {
+    return (
+        <>
+        
+        <h1>Hello, India Cohort!</h1>
+        <h3>
+            The Trio Of Savages</h3>
+        <ul>
+            <li>Hiro
+            <li>Garry</li>
+            <li>Leo</li>
+             </li>
+        </ul>
+        
+    <h2>Top Five Animes</h2>
+    <ol>
+        <li>Death Note</li>
+        <li>One piece</li>
+        <li>Dragon Ball Z</li>
+        <li>Naruto</li>
+        <li>Attack On Titan</li>
+    </ol>
+    <footer>India Cohort</footer>
+        </>
+    )
+}
+export default Components
