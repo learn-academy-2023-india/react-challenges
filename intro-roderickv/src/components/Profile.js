570cd2ca41c1e9cae1bd8710f8a4b757fd25a523
RoderickVongsa <roderick.vongsa@gmail.com>
2023-12-04 12:10:39 -0800

    Top 5 anime list

---
+++ intro-roderickv/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <>
+      <h3>Information</h3>
+      <ul>
+        <li>Roderick</li>
+        <li>aka "Rod"</li>
+        <li>enjoyer of food</li>
+        <li>watcher of the anime</li>
+      </ul>
+    </>
+  )
+}
+
+export default Profile
