fdf8e242975c225ab22c0b770467021ea6e30cf4
mckenley66 <mckenleyscullarkjr@gmail.com>
2023-12-04 13:48:34 -0800

    completed react app challenge

---
+++ intro-mckenley-sakaa-andrew/src/components/favorite.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Favorite = () => {
+    return (
+       <>
+        <h3>Top 5 Movies</h3>
+        <ul> 
+            <li>Indiana Jones</li>
+            <li>Everything Everywhere All at Once</li>
+            <li>Forrest Gump</li>
+            <li>The Mummy</li>
+            <li>Equalizer</li>
+        </ul>
+        </>
+    ) 
+}
+
+export default Favorite
