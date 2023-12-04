bd9b5e5522bf1825b8eaa0c07bc27856ec4cc468
chantelcarter <chantel.alexia@gmail.com>
2023-12-04 11:14:39 -0800

    react app challenge

---
+++ intro-chantel-tomas-irvin/src/components/Favorite.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Favorite = () => {
+  return (
+    <>
+        <h3>Top 5 TV shows:</h3>
+        <ol>
+            <li>Lucifer</li>
+            <li>Snowfall</li>
+            <li>Better Call Saul</li>
+            <li>Mayans</li>
+            <li>The Office</li>
+        </ol>
+    </>
+  )
+}
+
+export default Favorite
