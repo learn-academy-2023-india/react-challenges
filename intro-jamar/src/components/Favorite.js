601ad17bca599e3732a21e5f34e6e984b28bd906
jwat5623 <jamar34watson56@gmail.com>
2023-12-04 14:19:18 -0800

    adds react app

---
+++ intro-jamar/src/components/Favorite.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Favorite = () => {
+  return (
+    <>
+      <h1>Top Five Favorite Restaurant</h1>
+      <ol> 
+        <li>Tropicalia</li>
+        <li>Chick Fil A</li>
+        <li>Jakes Wayback Burgers</li>
+        <li>Waffle House</li>
+        <li>Cook Out</li>
+      </ol>
+      <h1>Top Five Favorite Sports</h1>
+      <ol>
+      <li>Basketball</li>
+      <li>Football</li>
+      <li>Track and Field</li>
+      <li>Soccer</li>
+      <li>Baseball</li>
+      </ol>
+    </>
+  )
+}
+
+export default Favorite
