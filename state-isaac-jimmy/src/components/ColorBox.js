e870a842c1323e7c69a8d01056f44030ce463adc
vanmeciv <vanmeciv@gmail.com>
2023-12-05 12:21:29 -0800

    Colorbox w/ random color & button - Isaac & Jimmy

--- state-isaac-jimmy/src/components/ColorBox.js
+++ state-isaac-jimmy/src/components/ColorBox.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react"
+import ColorString from "./ColorString"
 
 const ColorBox = () => {
   const [color, setColor] = useState(0)
   
 
   const handleClick = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16)
-    document.body.style.backgroundColor = "#" + randomColor
+    const randomNum = Math.floor(Math.random()*16777215).toString(16)
+    setColor("#" + randomNum)
+    
   }
 
   return (
-    <div onClick={handleClick} className="square" style = { {square: color} }>
-       
+    <div onClick={handleClick} className="square" style = { {backgroundColor: color} }>
+    <button onClick = {handleClick} >
+    #randomNum
+    </button>
     </div>
+    
   )
 }
 
