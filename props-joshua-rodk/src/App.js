2e57cfcd63dee6751013afef5a274b7f77673fd1
RoderickVongsa <roderick.vongsa@gmail.com>
2023-12-07 16:45:19 -0800

    Got to second story

--- props-joshua-rodk/src/App.js
+++ props-joshua-rodk/src/App.js
@@ -1,70 +1,37 @@
-// import React, { useState } from "react"
-// import Greeter from "./components/Greeter"
-// import AddPerson from "./components/AddPerson"
-// import "./App.css"
-
-// const App = () => {
-//   const [people, setPeople] = useState(["Hamilton", "Washington", "Jefferson"])
-
-//   const addPerson = () => {
-//     const newPerson = prompt()
-//     setPeople([...people, newPerson])
-//   }
-
-//   return (
-//     <>
-//       <h1>Greeter Application</h1>
-//       {people.map((person, index) => {
-//         return <Greeter name={person} key={index} />
-//       })}
-//       <AddPerson addPerson={addPerson} />
-//     </>
-//   )
-// }
-
-// export default App
-
-import React, { useState } from "react"
-import Episode from "./components/Episode"
-import MediaPlayer from "./components/MediaPlayer"
-import "./App.css"
+import React, { useState } from "react";
+import Dice from "./components/Dice";
+import MediaPlayer from "./components/MediaPlayer";
+import "./App.css";
 
 const App = () => {
-  const episodes = [
-    "Dice!", "Pool"
-    
-  ]
-  const [currentEpisode, setCurrentEpisode] = useState(episodes[0])
-  // console.log(currentEpisode)
-  const handleNextEpisode = () => {
-    // setCurrentEpisode(currentEpisode + 1)
-    // --> CSS Rocks!1
-    // --> "CSS Rocks!" + 1
-    const nextIndex = episodes.indexOf(currentEpisode) + 1
-    setCurrentEpisode(episodes[nextIndex])
-  }
+  const dicegame = [
+    "https://raw.githubusercontent.com/learn-academy-2023-india/syllabus/main/react/assets/1-dice.png",
+    "https://raw.githubusercontent.com/learn-academy-2023-india/syllabus/main/react/assets/2-dice.png",
+    // Add more dice images as needed
+  ];
+
+  const [currentDice, setCurrentDice] = useState(dicegame[0]);
+
+  const handleNextDice = () => {
+    const nextIndex = dicegame.indexOf(currentDice) + 1;
+    setCurrentDice(dicegame[nextIndex % dicegame.length]);
+  };
+
+  const handleCardClick = () => {
+    // Cycle to the next dice image
+    const nextIndex = dicegame.indexOf(currentDice) + 1;
+    setCurrentDice(dicegame[nextIndex % dicegame.length]);
+  };
 
   return (
     <>
       <h1>Our Work Example</h1>
-      <MediaPlayer
-        currentEpisode={currentEpisode}
-        handleNextEpisode={handleNextEpisode}
-      />
+      <MediaPlayer currentDice={currentDice} handleNextDice={handleNextDice} />
       <div className="cards">
-        {episodes.map((episode, index) => {
-          return <Episode title={episode} key={index} />
-        })}
-
-        {/* <Episode title={episodes[0]} />
-        <Episode title={episodes[1]} />
-        <Episode title={episodes[2]} />
-        <Episode title={episodes[3]} />
-        <Episode title={episodes[4]} />
-        <Episode title={episodes[5]} /> */}
+        <Dice title={currentDice} onClick={handleCardClick} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- props-joshua-rodk/src/components/MediaPlayer.js
+++ props-joshua-rodk/src/components/MediaPlayer.js
@@ -1,25 +1,11 @@
-// import React from "react"
-
-// const Greeter = (props) => {
-//   return (
-//     <>
-//       <h3>Hello {props.name}!</h3>
-//     </>
-//   )
-// }
-
-// export default Greeter
-
 import React from "react"
 
 const MediaPlayer = (props) => {
-  // console.log(props)
-  // props = {currentEpisode: 'CSS Rocks!'}
-  // props = {currentEpisode: 'CSS Rocks!', handleNextEpisode: ƒ}
+  
   return (
     <>
-      <h3>Current episode: {props.currentEpisode}</h3>
-      <button onClick={props.handleNextEpisode}>Dice butt episode</button>
+      
+      
 
       <br />
       <br />
