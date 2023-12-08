56593a0e36f876fa4e949142e9aa2089ebe28e70
Keoni <joonhyuk5972@gmail.com>
2023-12-07 14:46:33 -1000

    42

---
+++ props-brandon-keoni/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+// function App() {
+//   return 
+// }
+
+// export default App;
+
+import React from "react"
+import Dice from "./components/RollDice"
+import "./App.css"
+const App = () => {
+  return (
+    <>
+      <Dice />
+    </>
+  )
+}
+export default App
