cb109995bd0907dc290ec43fecb3791899ab634c
user.email <learnacademy@LEARNs-MacBook-Air.local>
2023-12-11 16:36:27 -0800

    cleans app.js

--- functional-props-jeremie-andrew/src/App.js
+++ functional-props-jeremie-andrew/src/App.js
@@ -5,36 +5,37 @@ import "./App.css"
 
 const App = () => {
   const [food, setFood] = useState(MenuUI())
+  // stretch goals
   const [totalCost, setTotalCost] = useState(0)
 
   const handleSelect = (selectedItem) => {
     const updatedFood = food.map((foodItem) => {
       if (foodItem.item === selectedItem) {
-        const selected = !foodItem.selected;
+        const selected = !foodItem.selected
         return { ...foodItem, selected }
       } else {
         return { ...foodItem }
       }
-    });
+    })
     setFood(updatedFood)
     updateTotalCost(updatedFood)
-  };
+  }
 
   const updateTotalCost = (updatedFood) => {
-    let baseTotal = 0;
+    let baseTotal = 0
     for (const foodItem of updatedFood) {
       if (foodItem.selected) {
-        baseTotal += foodItem.price;
+        baseTotal += foodItem.price
       }
     }
 
     // 8% sales tax
-    const salesTaxRate = 0.08; 
+    const salesTaxRate = 0.08
     const totalWithTax = baseTotal * (1 + salesTaxRate)
     setTotalCost(totalWithTax)
   }
 
-  const selectedFood = food.filter((foodItem) => foodItem.selected);
+  const selectedFood = food.filter((foodItem) => foodItem.selected)
 
   return (
     <>
@@ -42,7 +43,7 @@ const App = () => {
       <h1>Food Ordering App</h1>
       <div className="menu-container">
         {food.map((foodItem, index) => {
-          return <OrderUI key={index} food={foodItem} onSelect={handleSelect} />;
+          return <OrderUI key={index} food={foodItem} onSelect={handleSelect} />
         })}
       </div>
       <div className="selected-items-container">
